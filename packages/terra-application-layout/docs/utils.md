43e03e99a2fcc12eca318a497bb80ffec64b32f7
Biethman,Tyler <Tyler.Biethman@Cerner.com>
2018-03-16 11:33:12 -0500

    Adding utils doc.

---
+++ packages/terra-application-layout/docs/utils.md
@@ -0,0 +1,2 @@
+# Terra Application Layout
+
--- packages/terra-application-layout/examples/Utils.site-page.jsx
+++ packages/terra-application-layout/examples/Utils.site-page.jsx
@@ -1,30 +1,13 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React from 'react';
-import PropsTable from 'terra-props-table';
 import Markdown from 'terra-markdown';
-import ReadMe from '../docs/README.md';
+import UtilsDoc from '../docs/utils.md';
 import { version } from '../package.json';
-
-// Component Source
-/* eslint-disable import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions */
-import ApplicationLayoutSrc from '!raw-loader!../src/ApplicationLayout.jsx';
-/* eslint-enable import/no-webpack-loader-syntax, import/first, import/no-unresolved, import/extensions */
-
-// Example Files
-
-import ExampleApplication from './index-examples/ExampleApplication';
 
 const ApplicationExamples = () => (
   <div>
     <div id="version">Version: {version}</div>
-    <Markdown id="readme" src={ReadMe} />
-    <h2>Examples</h2>
-    <div style={{ height: '700px' }}>
-      <ExampleApplication />
-    </div>
-    <br />
-    <br />
-    <PropsTable id="props-header" src={ApplicationLayoutSrc} componentName="ApplicationLayout" />
+    <Markdown id="utils" src={UtilsDoc} />
   </div>
 );
 
