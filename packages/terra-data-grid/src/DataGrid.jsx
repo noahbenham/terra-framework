3ade8d868809072b57f037cd1253b9ecbedd877c
tbiethman <tbiethman@gmail.com>
2018-03-29 15:24:57 -0500

    super derp

--- packages/terra-data-grid/src/DataGrid.jsx
+++ packages/terra-data-grid/src/DataGrid.jsx
@@ -24,7 +24,7 @@ const columnSet = (num) => {
   const columns = [];
   for (let i = 0; i < num; i += 1) {
     columns.push((
-      <Column name={`Column ${i}`} cellCount={1000} />
+      <Column name={`Column ${i}`} cellCount={50} />
     ));
   }
   return columns;
@@ -34,7 +34,8 @@ class DataGrid extends React.Component {
   constructor(props) {
     super(props);
 
-    this.updateRefs = this.updateRefs.bind(this);
+    this.handleScrollFill = this.handleScrollFill.bind(this);
+    this.handleScrollFixed = this.handleScrollFixed.bind(this);
 
     this.state = {
 
@@ -42,31 +43,53 @@ class DataGrid extends React.Component {
   }
 
   componentDidMount() {
-    this.updateRefs();
+    this.filledRef.addEventListener('scroll', this.handleScrollFill);
+    this.fixedRef.addEventListener('scroll', this.handleScrollFixed);
   }
 
-  updateRefs() {
-    if (this.fixedRef) {
-      this.fixedRef.style.height = `${this.fixedRef.scrollHeight}px`;
+  componentWillUnmount() {
+    this.filledRef.removeEventListener('scroll', this.handleScrollFill);
+    this.fixedRef.removeEventListener('scroll', this.handleScrollFixed);
+  }
 
-      if (this.filledRef) {
-        this.filledRef.style.height = `${this.fixedRef.scrollHeight}px`;
-      }
+  handleScrollFill(event) {
+    if (!this.updatingFill) {
+      this.updatingFixed = true;
+      this.fixedRef.scrollTop = this.filledRef.scrollTop;
     }
+    this.updatingFill = false;
   }
 
-  componentDidUpdate() {
-    this.updateRefs();
+  handleScrollFixed(event) {
+    if (!this.updatingFixed) {
+      this.updatingFill = true;
+      this.filledRef.scrollTop = this.fixedRef.scrollTop;
+    }
+    this.updatingFixed = false;
   }
+
+  // updateRefs() {
+  //   if (this.fixedRef) {
+  //     this.fixedRef.style.height = `${this.fixedRef.scrollHeight}px`;
+
+  //     // if (this.filledRef) {
+  //     //   this.filledRef.style.height = `${this.fixedRef.scrollHeight}px`;
+  //     // }
+  //   }
+  // }
+
+  // componentDidUpdate() {
+  //   this.updateRefs();
+  // }
 
   render() {
     return (
-      <div className={cx(['container', 'container-size-1'])}>
+      <div className={cx(['container', 'container-size-2'])}>
         <div ref={(fixedRef) => { this.fixedRef = fixedRef; }} className={cx('fixed')}>
           {columnSet(3)}
         </div>
         <div ref={(filledRef) => { this.filledRef = filledRef; }} className={cx('fill')}>
-          {columnSet(30)}
+          {columnSet(100)}
         </div>
       </div>
     );
